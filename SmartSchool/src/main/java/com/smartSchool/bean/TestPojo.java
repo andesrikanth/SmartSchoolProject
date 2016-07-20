1db941eec80e92005bee9302ecded250ab64dfe5
srande <srande@SRANDE-IN.us.oracle.com>
2016-07-19 20:01:10 -0600

    Created Managed Beans xml file for maintaining all Bean classes.
    --Srikanth

--- SmartSchool/src/main/java/com/smartSchool/bean/TestPojo.java
+++ SmartSchool/src/main/java/com/smartSchool/bean/TestPojo.java
@@ -5,12 +5,12 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean(name = "testBean", eager = true)
-@SessionScoped
+//@ManagedBean(name = "testBean", eager = true)
+//@SessionScoped
 public class TestPojo implements Serializable {
 private static final long serialVersionUID = 1L;
 	
-	private String name="Srikanth";
+	private String name="Srikanth Test";
 
 	public String getName() {
 		return name;
--- SmartSchoolService/src/main/java/com/smartSchoolService/bean/TestPojo.java
+++ SmartSchoolService/src/main/java/com/smartSchoolService/bean/TestPojo.java
@@ -5,12 +5,12 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean(name = "testServiceBean", eager = true)
-@SessionScoped
+//@ManagedBean(name = "testServiceBean", eager = true)
+//@SessionScoped
 public class TestPojo implements Serializable {
 private static final long serialVersionUID = 1L;
 	
-	private String name="Srikanth Service";
+	private String name="Srikanth Service Test";
 
 	public String getName() {
 		return name;
